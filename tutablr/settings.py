051718559c084c49736a851bbf5d8bf036df9c73
Ashwin Ramesh <ashwinramesh92@hotmail.com>
2012-10-12 22:06:12 +1100

    added bootstrap to static. trying to clean up registration templates

--- tutablr/settings.py
+++ tutablr/settings.py
@@ -66,6 +66,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(DIRNAME,'../static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -108,7 +109,9 @@ WSGI_APPLICATION = 'tutablr.wsgi.application'
 
 TEMPLATE_DIRS = (
     os.path.join(DIRNAME,'../templates'),
-    os.path.join(DIRNAME,'..'),
+    os.path.join(DIRNAME,'../registration/templates'),
+    os.path.join(DIRNAME,'../dajaxice/templates'),
+    os.path.join(DIRNAME,'../tutablr_app/templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
