7dfeb8cdcc6acdbf5fa46433752f55f277f94c61
Luke Leslie <LukeLeslie@vlan949-172-24-13-87.wireless.usyd.edu.au>
2012-10-20 13:35:44 +1100

    removing ENV...

--- ENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/LukeLeslie/Desktop/tutablr_main/luke_calendar_branch/ENV/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
