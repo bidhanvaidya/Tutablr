bbab9f19ac4eb99d1736b98c02e0e54ee02b4512
Ashwin Ramesh <ashwinramesh92@hotmail.com>
2012-10-23 20:21:50 +1100

    working infobox in gmaps

--- googlemaps/gmaps.js
+++ googlemaps/gmaps.js
@@ -1,5 +1,7 @@
   var geocoder;
   var map;
+  var marker;
+  var infowindow = new google.maps.InfoWindow();
   function initialize() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(-34.397, 150.644);
@@ -18,8 +20,16 @@
         map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
             map: map,
-            position: results[0].geometry.location
+            position: results[0].geometry.location,
+            title: "Hello World"
+        }); 
+            marker['infowindow'] = new google.maps.InfoWindow({
+            content: "TEST"
         });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        this['infowindow'].open(map, this);
+    });   
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
